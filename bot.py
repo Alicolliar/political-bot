431b3a49dd726c588005a0ba97e7fab9dc91a2a8
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-04-21 11:09:43 +0100

    autorole

--- bot.py
+++ bot.py
@@ -7,6 +7,19 @@ bot = commands.Bot("-")
 async def battlesceptre(ctx, who: discord.Member):
     await ctx.send(f"{ctx.author.mention} has used a battle sceptre on {who.mention}! Oh no!")
 
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    role = guild.get_role(559123130191446028)
+    await member.add_roles(role)
+    await guild.get_channel(559122949140119553).send(f"Welcome to Britain, {member.mention}, I hope you had a pleasant flight. But before you can pass you must answer me these riddles three: do you have rabies, what is your favourite hot beverage and what condiment do you have chips with?")
+
+
 if __name__ == "__main__":
-    from sys import argv
-    bot.run(argv[1])
+    from os import environ
+    import sys
+    token = environ.get("BOT_TOKEN", None)
+    if token is None:
+        sys.exit("Please set envvar BOT_TOKEN to your token.")
+    else:
+        bot.run(token)
