0ca0f191aabfed262cad9cc61f2efdf5d67746bf
Alicolliar <alicolliar@gmail.com>
2019-12-19 18:16:19 +0000

    Updated voting
    Added channel specific stuff

--- bot.py
+++ bot.py
@@ -62,18 +62,22 @@ async def startvote(ctx, place, subject):
     if speaker in ctx.author.roles:
         if place == "public":
             await ctx.guild.get_channel(654305941331902511).send(f"The Speaker has started a vote on the subject of "+subject)
+            await ctx.guild.get_channel(657273438796513310).send(f"A vote has been started on the subject of "+subject+". Please vote using the relevant bot commands.")
             global voteStatus
             voteStatus = [1,0,0]
         elif place == "commons":
             await ctx.guild.get_channel(654337039445000201).send(f"The Speaker has started a vote on the subject of "+subject)
+            await ctx.guild.get_channel(649879662092222464).send(f"A vote has been started for "+subject+". Please vote using the relevant bot commands.")
             voteStatus = [0,1,0]
         elif place == "lords":
             await ctx.guild.get_channel(654337184429768740).send(f"The Speaker has started a vote on the subject of "+subject)
+            await ctx.guild.get_channel(649879745189904384).send(f"A vote has been started for "+subject+". Please vote using the relevant bot commands.")
             voteStatus = [0,0,1]
         else:
             await ctx.send(f"Sorry, Speaker, but you can't set up a vote for that group.")
     else:
         await ctx.send(f"Sorry, but only the Speaker can set up votes.")
+
 
 
 @bot.command()
@@ -88,21 +92,21 @@ async def vote(ctx, vote):
         else:
             await ctx.send(f"Thank you {ctx.author.mention}, your vote has been registered.")
             await ctx.guild.get_channel(654305941331902511).send(f"**One** Vote has been registered for: "+vote)
-            await ctx.delete_message(ctx)
+            await ctx.Messge.delete()
     elif voteStatus[0] == 0 and voteStatus[1] == 1 and voteStatus[2] == 0:
         if mp not in ctx.author.roles:
             await ctx.send(f"You are unable to take part in this vote. If you believe you have been disenfranchised, please talk to any government employee.")
         else:
             ctx.send(f"Thank you {ctx.author.mention}, your vote has been registered.")
             await ctx.guild.get_channel(654337039445000201).send(f"**One** Vote has been registeredd for: "+vote)
-            await ctx.	xdelete_message(ctx)
+            await ctx.Message.delete()
     elif voteStatus[0] == 0 and voteStatus[1] == 0 and voteStatus[2] == 1:
         if lord not in ctx.author.roles:
             await ctx.send(f"You are unable to take part in this vote. If you believe you have been disenfranchised, please talk to any government employee.")
         else:
             await ctx.send(f"Thank you {ctx.author.mention}, your vote has been registered.")
             await ctx.guild.get_channel(654337184429768740).send(f"**One** Vote has been registered for: "+vote)
-            await delete_message(ctx)
+            await ctx.Message.delete()
     else:
         await ctx.send(f"There is no vote happening right now.")
 
@@ -111,8 +115,20 @@ async def endvote(ctx):
     """Used by the speaker to end elections/votes."""
     speaker = ctx.guild.get_role(575355569712398348)
     if speaker in ctx.author.roles:
-        global voteStatus
-        voteStatus = [0,0,0]
+        if voteStatus == [1,0,0]:
+            await ctx.guild.get_channel(657273438796513310).send(f"The election has ended, the results will be announced shortly.")
+            await ctx.guild.get_channel(654305941331902511).send(f"The election ended.")
+            voteStatus = [0,0,0]
+        elif voteStatus == [0,1,0]:
+            await ctx.guild.get_channel(649879662092222464).send(f"The vote has ended, the result will be announced shortly.")
+            await ctx.guild.get_channel(654337039445000201).send(f"The vote ended.")
+            voteStatus = [0,0,0]
+        elif voteStatus = [0,0,1]
+            await ctx.guild.get_channel(649879745189904384).send(f"The vote has ended, the results will be announced shortly.")
+            await ctx.guild.get_channel(654337184429768740).send(f"The vote ended.")
+            voteStatus = [0,0,0]
+       else:
+            await ctx.send(f"There is not a vote to end, Speaker.")
     else:
         await ctx.send(f"Sorry, but only the Speaker can end votes.")
 
